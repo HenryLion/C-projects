097adce9c81cd910b4754e4504228dd76ac87b6a
HenryLion <anbreakdy@outlook.com>
2019-12-15 17:26:45 +0800

    add: get value from postfix expression and handle spaces

--- datastruct/stack/postfix_expression.c
+++ datastruct/stack/postfix_expression.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "stack.h"
 
 /* 本程序利用栈求得运算式的后缀形式，并根据其后缀形式求值 */
 /* 本程序假定输入的运算式都是合法的 */
+
+/* 编译运行方法
+compile: gcc postfix_expression.c stack.c
+
+hanb@:stack$ ./a.out 
+Input the expression (End with '#'): 1 * 2 + (3-9 / 2) * 2   #
+postfix expression is: 12*392/-2*+#
+Calculation result is : -1.000000.
+*/
+
+/****************************************************
+ * function: 获取键盘输入的表达式字串
+ * author: herbert
+ * date: 2019-12-10
+ ****************************************************/
+void get_input (char *out)
+{
+	int idx = 0;
+	char ch;
+	while ( ( ch = getchar () ) != EOF)
+	{
+		if (ch == '\r' || ch == '\n')
+			break;
+		out[idx++] = ch;
+	}
+	return;
+}
 
 /****************************************************
  * function: 判断是否是操作符，#也看作操作符
@@ -56,6 +84,11 @@ int get_postfix_expression (const char *ori_expression, char *result)
 
 	while ('#' != *p)
 	{
+		if (' ' == *p || '\t' == *p)
+		{
+			++p;
+			continue;
+		}
 		if (!is_operator(*p)) // 不是操作符号
 		{
 			if ( '(' == *p ) // 左括号入栈
@@ -98,16 +131,91 @@ int get_postfix_expression (const char *ori_expression, char *result)
 		if ('#' != pop_ch)
 			result[rst_idx++] = pop_ch;
 	}
+	result[rst_idx] = '#';
 
+	clear_stack (stack_op);
 	return 0;
 }
 
-
-int main (void)
+/****************************************************
+ * function: 根据操作符计算结果
+ * author: herbert
+ * date: 2019-12-10
+ * common: ab*cde/-f*+
+ ****************************************************/
+float get_value (float first_val, float second_val, char op)
 {
+	switch (op)
+	{
+		case '+':
+			return first_val + second_val;
+		case '-':
+			return first_val - second_val;
+		case '*':
+			return first_val * second_val;
+		case '/':
+			return first_val / second_val;
+		default:
+			printf ("get_value: invalid op\n");
+			exit (-1);
+	}
+}
+
+/****************************************************
+ * function: 根据后缀表达式求值
+ * author: herbert
+ * date: 2019-12-10
+ * common: ab*cde/-f*+
+ ****************************************************/
+int get_value_by_postfix_expression (const char *post_ex, float *ret)
+{
+	if (NULL == post_ex || NULL == ret)
+		return -1;
+	Stack *stack = NULL;
+	init_stack (&stack);
+	const char *p = post_ex;
+	float push_val = 0.0;
+	float first_op_val = 0.0;
+	float second_op_val = 0.0;
+	float temp_ret = 0.0;
+	while (*p != '#')
+	{
+		if (!is_operator(*p))
+		{
+			push_val = (*p - '0') * 1.0;
+			push (stack, &push_val, sizeof(float));
+		}
+		else
+		{
+			if (stack->top < 1)
+			{
+				printf ("invalid postfix expression\n");
+				return -1;
+			}
+			pop (stack, &second_op_val, sizeof (float));
+			pop (stack, &first_op_val, sizeof (float));
+			temp_ret = get_value (first_op_val, second_op_val, *p);
+			push (stack, &temp_ret, sizeof (float));
+		}
+		++p;
+	}
+
+	// 最终的结果就是栈顶唯一的元素，取出它到ret
+	pop (stack, ret, sizeof (float));
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+	printf ("Input the expression (End with '#'): ");
+	char input_str[100] = {0};
+	get_input (input_str);
 	char postfix_expression [100] = {0};
-	get_postfix_expression ("a*b+(c-d/e)*f#", postfix_expression);
+	get_postfix_expression (input_str, postfix_expression);
 	printf ("postfix expression is: %s\n", postfix_expression);
+	float result = 0.0;
+	get_value_by_postfix_expression (postfix_expression, &result);
+	printf ("Calculation result is : %f.\n", result);
 	return 0;
 }
 
