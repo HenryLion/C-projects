05f556bd73295e5a9bca588eb7ff4e377b74f6b3
HenryLion <anbreakdy@outlook.com>
2019-12-27 23:59:57 +0800

    commit matrix transpose

--- datastruct/sparse_array/sparse_array.c
+++ datastruct/sparse_array/sparse_array.c
@@ -40,13 +40,12 @@ Sparse_matrix* create_sparse_matrix (Sparse_matrix **s_matrix)
 		return NULL;
 
 	memset (*s_matrix, 0, sizeof (Sparse_matrix));
-
 	return *s_matrix;
 }
 
 
 /**********************************************
-* function: 判断一个坐标在矩阵中是否已经存在
+* function: 判断一个行列坐标元素在矩阵中是否已经存在
 * author: Herbert
 * date: 2019-12-26
 **********************************************/
@@ -133,7 +132,7 @@ void init_two_dimensional_array (int *arr)
 {
 	if (NULL == arr)
 		return ;
-	int non_zero_num = (ROW*COL) / 20;
+	int non_zero_num = (ROW*COL) / 20 + 5; 
 	int i;
 	for (i = 0; i < non_zero_num; ++i)
 	{
@@ -171,25 +170,143 @@ void get_sparse_matrix_from_normal_matrix (int *arr, int row, int col, Sparse_ma
 }
 
 
+/**********************************************
+* function: 实现一个稀疏矩阵的转置运算
+* author: Herbert
+* date: 2019-12-27
+**********************************************/
+Sparse_matrix * sparse_matrix_transpose (Sparse_matrix * s_matrix)
+{
+	if (NULL == s_matrix)
+		return NULL;
+	Sparse_matrix *trans_matrix = (Sparse_matrix*) malloc (sizeof (Sparse_matrix));	
+	if (NULL == trans_matrix)
+		return NULL;
+	trans_matrix->row = s_matrix->col;
+	trans_matrix->col = s_matrix->row;
+	trans_matrix->node_num = s_matrix->node_num;
+
+	int i,j;
+	int node_pos = 0;
+	for (i = 0; i < s_matrix->node_num; ++i)
+	{
+		node_pos = 0;
+		for (j = 0; j < s_matrix->node_num; ++j)
+		{
+			if (i == j)
+				continue;
+			if (s_matrix->nodes[i].c_idx > s_matrix->nodes[j].c_idx)
+			{
+				++node_pos;
+			}
+			else if (s_matrix->nodes[i].c_idx == s_matrix->nodes[j].c_idx)
+			{
+				if (s_matrix->nodes[i].r_idx > s_matrix->nodes[j].r_idx)
+					++node_pos;
+			}
+			else
+			{
+				// do nothing;
+			}
+		}
+		trans_matrix->nodes[node_pos].r_idx = s_matrix->nodes[i].c_idx;
+		trans_matrix->nodes[node_pos].c_idx = s_matrix->nodes[i].r_idx;
+		trans_matrix->nodes[node_pos].value = s_matrix->nodes[i].value;
+	}
+	return trans_matrix;
+}
+
+/**********************************************
+* function: 实现一个稀疏矩阵的转置运算(优化方法)
+* author: Herbert
+* date: 2019-12-27 严蔚敏视频14前10分钟
+**********************************************/
+Sparse_matrix * optimize_sparse_matrix_transpose (Sparse_matrix *s_matrix)
+{
+	if (NULL == s_matrix)
+		return NULL;
+	Sparse_matrix *trans_matrix = (Sparse_matrix*) malloc (sizeof (Sparse_matrix));	
+	if (NULL == trans_matrix)
+		return NULL;
+	trans_matrix->row = s_matrix->col;
+	trans_matrix->col = s_matrix->row;
+	trans_matrix->node_num = s_matrix->node_num;
+	int i,node_pos;
+	// 求原矩阵中每列元素个数并记录在数组中
+	int *col_num = (int *)malloc (s_matrix->col * sizeof (int));
+	if (col_num == NULL)
+		return NULL;
+	for (i = 0; i < s_matrix->node_num; ++i)
+	{
+		col_num[s_matrix->nodes[i].c_idx]++;
+	}
+
+	printf ("col_num: ");
+	for (i = 0; i < s_matrix->col; ++i)
+		printf ("%d ", col_num[i]);
+
+	// 求原矩阵中每列首元素（对应转置矩阵每行首元素）在转置存储结构中的位置
+	int *row_first_pos = (int*)malloc (s_matrix->col * sizeof (int));
+	if (row_first_pos == NULL)
+		return NULL;
+	row_first_pos[0] = 0;
+	for (i = 1; i < s_matrix->col; ++i)
+	{
+		row_first_pos[i] = row_first_pos[i-1] + col_num[i-1];
+	}
+	printf ("row_first_pos: ");
+	for (i = 0; i < s_matrix->col; ++i)
+		printf ("%d ", row_first_pos[i]);
+	
+	// 遍历原矩阵，将每个node放入转置矩阵的对应位置
+	for (i = 0; i < s_matrix->node_num; ++i)
+	{	
+		node_pos = row_first_pos[s_matrix->nodes[i].c_idx];
+		trans_matrix->nodes[node_pos].r_idx = s_matrix->nodes[i].c_idx;
+		trans_matrix->nodes[node_pos].c_idx = s_matrix->nodes[i].r_idx;
+		trans_matrix->nodes[node_pos].value = s_matrix->nodes[i].value;
+		row_first_pos[s_matrix->nodes[i].c_idx]++;
+	}
+
+	free (col_num);
+	free (row_first_pos);
+	return trans_matrix;
+}
+
 
 int main (void)
 {
 	srand (time(NULL));
 
 	int arr[ROW*COL] = {0};
-
+	
+	// 初始化二维数组元素值
 	init_two_dimensional_array (arr);
 
 	printf ("\nsrc matrix is: \n");	
 	print_two_dimensional_array (arr, ROW, COL);
+
+	// 使用普通方式计算matrix的转置
 	int *transpose_arr = matrix_transpose (arr, ROW, COL);
 	printf ("\ntranspose matrix is:\n");
 	print_two_dimensional_array (transpose_arr, COL, ROW);
 
 	Sparse_matrix *s_matrix = NULL;
 	create_sparse_matrix (&s_matrix);
+
+	// 从普通矩阵表示法得到稀疏矩阵表示结构
 	get_sparse_matrix_from_normal_matrix (arr, ROW, COL, s_matrix);
 	print_sparse_matrix (s_matrix);
+	
+	// 自己实现的稀疏矩阵转置
+	Sparse_matrix *transpos_matrix = sparse_matrix_transpose (s_matrix);
+	print_sparse_matrix (transpos_matrix);
 
+	Sparse_matrix *opti_transpose_matrix = optimize_sparse_matrix_transpose (s_matrix);
+	print_sparse_matrix (opti_transpose_matrix);
+
+	free (opti_transpose_matrix);
+	free (transpos_matrix);
+	free (s_matrix);
 	return 0;
 }
